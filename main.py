e85b50a72613134e315b2cde2ceabc4ef70a11c9
Fábio Oliveira <fabio.oliveira1403@outlook.com>
2023-09-13 20:31:33 -0300

    Desisto

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import Grafo
 
 if __name__ == '__main__':
     # Cria um digrafo com 5 vértices
-    digrafo = Grafo.Digrafo(4)
+    digrafo = Grafo.Digrafo(6)
 
     # Adiciona algumas arestas e seus respectivos pesos
     # digrafo.adicionar_aresta(0, 1, 3)
@@ -17,30 +17,25 @@ if __name__ == '__main__':
     #C: 2
     #D: 3
 
-    digrafo.adicionar_aresta(0, 1, 3)
-    digrafo.adicionar_aresta(1, 0, 8)
-    digrafo.adicionar_aresta(2, 0, 5)
-    digrafo.adicionar_aresta(0, 3, 7)
-    digrafo.adicionar_aresta(3, 0, 2)
-    digrafo.adicionar_aresta(1, 2, 2)
-    digrafo.adicionar_aresta(2, 3, 1)
+    digrafo.adicionar_aresta(0, 1, 2)
+    digrafo.adicionar_aresta(1, 2, 5)
+    digrafo.adicionar_aresta(2, 3, 3)
+    digrafo.adicionar_aresta(3, 4, 4)
+    digrafo.adicionar_aresta(4, 5, 7)
+    digrafo.adicionar_aresta(5, 0, 2)
+    digrafo.adicionar_aresta(0, 2, 5)
+    digrafo.adicionar_aresta(0, 3, 2)
+    digrafo.adicionar_aresta(0, 4, 2)
+    digrafo.adicionar_aresta(1, 3, 8)
+    digrafo.adicionar_aresta(1, 4, 4)
+    digrafo.adicionar_aresta(1, 5, 9)
+    digrafo.adicionar_aresta(2, 4, 2)
+    digrafo.adicionar_aresta(2, 5, 6)
+    digrafo.adicionar_aresta(3, 5, 5)
 
     # Exibe a matriz de adjacência do digrafo
     print("Matriz de Adjacência:")
-    digrafo.exibir()
 
-    # Verifica e exibe se o digrafo possui loops
-    digrafo.possui_loops()
 
-    # Verifica e exibe se o digrafo possui arestas paralelas
-    digrafo.possui_arestas_paralelas()
-
-    # Calcula e exibe os graus de entrada de todos os vértices
-    print("Graus de entrada:", digrafo.grau_entrada())
-
-    # Calcula e exibe os graus de saída de todos os vértices
-    print("Graus de saída:", digrafo.grau_saida())
-
-    # Executa o algoritmo de Floyd-Warshall e exibe a matriz de distâncias mais curtas
     digrafo.floyd_warshall()
 
